fb37f20bbb382cc7fbf2dd2891e2fc7d3f9fd388
Sid <39922327+ranasid17@users.noreply.github.com>
2020-08-29 17:21:32 -0500

    Add files via upload
    Push Euler's Method for solving 1st order ODEs

---
+++ eulerMethod.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 27 17:08:19 2020
+
+@author: Sid
+"""
+
+import numpy as np 
+
+# Euler's Method Implementation for Partitioned ODEs
+# Inputs
+#   f : input function 1
+#   g : input function 2 
+#   q0, p0 : initial conditions for the respective functions 
+#   N : number of steps 
+# Outputs
+#   q, p : numerical approximation to solution 
+def euler(f, g, q0, p0, N):
+    h = 1 / N # Calculate step size (inverse of num steps)
+    q = np.zeros((N+1, np.size(q0))) # define array to hold ODE values
+    p = np.zeros((N+1, np.size(p0))) # "  " 
+    q[0] = q0 # store initial condition
+    p[0] = p0 # "   " 
+    # iteratively solve and store ODE
+    for n in range(N):
+        q[n+1] = q[n] + h*f(p[n])
+        p[n+1] = p[n] + h*g(q[n])
+    return q, p
